884e2b826fe9ee21131a81c10974be264197b2b0
Abby Asomani <63776659+abbyabridged@users.noreply.github.com>
2020-11-15 20:37:28 +0800

    Pseudo code

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,46 @@
+d3.json("samples.json", function(data) {
+    console.log(data);
+});
+
+// Display the default plot
+
+
+////////// BAR CHART ////////
+
+// Get sample_values for bar chart
+
+// Get otu_ids for labels
+
+// Get otu_labels for hovertext
+
+// Create trace for bar chart
+
+// Plot bar chart
+
+
+////////// BUBBLE CHART ///////////
+
+// Get otu_ids for x values
+
+// Get sample_values for y values
+
+// Use sample_values for marker size
+
+// Use otu_labels for text values
+
+
+///////// INFO BOX ////////
+
+
+// Needs to filter based on drop down box selection
+
+
+////// DROP DOWN BOX ///////
+
+// On change to the DOM, call getData()
+
+// Function called by DOM changes
+
+    // Call function to update the chart
+
+// Update the restyled plot's values
